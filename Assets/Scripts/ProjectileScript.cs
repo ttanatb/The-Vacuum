59d9f01db3500d981762288478333de328640733
Benson Sherman <bcs4985@rit.edu>
2017-10-11 11:07:03 -0400

    Worked on weapon control script;

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+    public float speed;
+    Vector3 position;
+    Vector3 Velocity;
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
