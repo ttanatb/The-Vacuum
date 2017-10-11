b88df4301c968758322e457f3ac5180a030f0e56
Tanat Boozayaangool <ttanatb@gmail.com>
2017-10-11 10:58:45 -0400

    Added HealthPack Collectible and its animations

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public abstract class Collectible : MonoBehaviour
+{
+
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+
+    }
+}
---
+++ Assets/Scripts/HealthPack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : Collectible
+{
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
