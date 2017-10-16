ee0ee06b6eb511281b983c3922be8e3dd9b57855
ttanatb <ttanatb@gmail.com>
2017-10-16 10:32:23 -0400

    Created map graph logic

---
+++ Assets/Scripts/MapGraph.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGraph : MonoBehaviour
+{
+    public GameObject[] roomPrefabs;
+
+    private MapNode startingNode;
+
+    private int nodeCount = 25;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        startingNode = new MapNode(Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)]).GetComponent<Room>());
+
+        MapNode currNode = startingNode;
+        for (int i = 1; i < nodeCount; i++)
+        {
+            currNode.exits[0] = new MapNode(Instantiate(roomPrefabs[Random.Range(0, roomPrefabs.Length)]).GetComponent<Room>());
+            Connect(currNode.data, 0, currNode.exits[0].data);
+
+            currNode = currNode.exits[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Connect(Room exitRm, int exitIndex, Room entranceRm)
+    {
+        entranceRm.entrance.position = exitRm.exits[exitIndex].position;
+        entranceRm.entrance.rotation = exitRm.exits[exitIndex].rotation;
+
+        entranceRm.RepositionToEntrance();
+    }
+}
+
+
+public class MapNode
+{
+    public Room data;
+
+    int exitCount;
+    public MapNode entrance;
+    public MapNode[] exits;
+
+    public MapNode(Room room)
+    {
+        data = room;
+        exitCount = room.exits.Length;
+        exits = new MapNode[exitCount];
+    }
+}
