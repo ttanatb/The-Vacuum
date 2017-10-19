51743e65f2b4fea021ac986126031a6700495ada
Aidan Markham <apm4337@rit.edu>
2017-10-18 22:29:53 -0400

    Added crab

---
+++ Assets/MoveWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWorld : MonoBehaviour {
+    public Vector3 dir;
+    public float speed;
+    public float acceleration;
+    public GameObject enableatEnd;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += dir * speed * Time.deltaTime;
+        speed += acceleration * Time.deltaTime;
+	}
+
+    void Stop()
+    {
+        enabled = false;
+        enableatEnd.SetActive(true);
+    }
+}
