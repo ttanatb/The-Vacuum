6897421a41a251ca43667dc58f3dc3e7feb3e6bc
MAIN\bcs4985 <bcs4985>
2017-11-02 14:39:44 -0400

    Added bullet Emission

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -9,9 +9,10 @@ public class ProjectileScript : MonoBehaviour
     public float lifeSpan;
     public int outDamage = 2;
     private Vector3 position;
-    private Vector3 velocity;
+    private Vector3 firstForward;
     private float timer;
     Rigidbody rigidBody;
+    public GameObject particleSystem;
 
 
     void Start()
@@ -27,6 +28,8 @@ public class ProjectileScript : MonoBehaviour
         rigidBody = gameObject.GetComponent<Rigidbody>();
         rigidBody.AddForce(gameObject.transform.forward * 10, ForceMode.Impulse);
 
+        firstForward = gameObject.transform.forward;
+      
     }
 
     void Update()
@@ -44,17 +47,23 @@ public class ProjectileScript : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
+        //making a particle system
+        GameObject tempParticleSystem = Instantiate(particleSystem,gameObject.transform);
 
+        tempParticleSystem.transform.forward = gameObject.transform.forward*-1;
 
-        //further coding will happen here with enemies, or we may want to handle that in enemy script
-        //For now, we destory the projectile
+        tempParticleSystem.transform.SetParent(null);
+        
+
+        //collision with an enemy
         if (collision.gameObject.tag == "Enemy")
         {
             EnemyScript eScript = collision.gameObject.GetComponent<EnemyScript>();
             if (eScript)
             {
                 eScript.TakeDamage(outDamage);
-                Destroy(gameObject);
+
+                
             }
         }
 
@@ -62,19 +71,21 @@ public class ProjectileScript : MonoBehaviour
         {
             RangedEnemyScript eScript = collision.gameObject.GetComponent<RangedEnemyScript>();
             eScript.TakeDamage(outDamage);
+            
+        
         }
 
         else if (collision.gameObject.tag == "Player" && gameObject.tag == "Enemy")
         {
             PlayerCombat pScript = collision.gameObject.GetComponent<PlayerCombat>();
             pScript.TakeDamage(outDamage, collision.transform.position);//have them take damage
-            Destroy(gameObject);
+
+            
 
         }
-        else if (collision.gameObject.tag == "Room")
-        {
-            Destroy(gameObject);
-        }
+        Destroy(tempParticleSystem, 1.5f);
+        //no matter what destroy at the end.
+        Destroy(gameObject);
 
     }
 }
